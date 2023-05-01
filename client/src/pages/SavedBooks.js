b529b7b08a4abaa74b346bb52174bab398aa1959
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-04-30 22:46:50 -0400

    deleted unsued lines of code

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,8 +1,6 @@
 
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
-
-// import { getMe, deleteBook } from '../utils/API';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 import { useQuery, useMutation } from '@apollo/client';
